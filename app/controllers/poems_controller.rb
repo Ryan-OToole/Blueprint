52db7cfbc0a63e0c834c427b43c829050d8ea8a2
github username <github email address>
2018-07-22 21:43:56 -0400

    updated poem controller backend with edit and delete

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,2 +1,39 @@
 class PoemsController < ApplicationController
+    before_action :set_poem, only: [:show,:update,:destroy]
+
+  def index
+    poems = Poem.all
+    render json: poems, status: 200
+  end
+
+  def show
+    @poem = Poem.find_by(id: params[:id])
+    render json: @poem
+  end
+
+  def create
+    poem = Poem.create(poem_params)
+    render json: poem, status: 201
+  end
+
+  def update
+    @poem.update(poem_params)
+    render json: @poem, status: 200
+  end
+
+  def destroy
+    poemId = @poem.id
+    @poem.destroy
+    render json: {message:"Zap! Poem b gone", poemId:poemId}
+  end
+
+  private
+    def poem_params
+      params.permit(:body, :title)
+    end
+
+    def set_poem
+      @poem = Poem.find(params[:id])
+    end
+
 end
